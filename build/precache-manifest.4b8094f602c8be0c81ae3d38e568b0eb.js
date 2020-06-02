b3630944eef108a70d35ceceb614ddb984e4d00e
alpiazza13 <alexpiazza@Alexs-MacBook-Pro-3.local>
2020-06-02 03:06:40 -0400

    uibuild

---
+++ build/precache-manifest.4b8094f602c8be0c81ae3d38e568b0eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa70742ea0eaea5d38857f2d756ab2c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73517b7aa9fc318508d7",
+    "url": "/static/css/main.4ac2ad13.chunk.css"
+  },
+  {
+    "revision": "c2db7b13b6c244651373",
+    "url": "/static/js/2.d5e710d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d5e710d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73517b7aa9fc318508d7",
+    "url": "/static/js/main.72cae5a2.chunk.js"
+  },
+  {
+    "revision": "68d5127cb03992670844",
+    "url": "/static/js/runtime-main.684b035f.js"
+  }
+]);
